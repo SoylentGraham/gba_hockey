7d8f4c5a1a68a900ec1f0d8216b87e156930bfad
Graham Reeves <graham@newchromantics.com>
2017-10-24 11:01:44 +0100

    echo build result for quick sanity check

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 ${DEVKITARM}/bin/arm-none-eabi-gcc -O3 -o hockey2.elf -T core/gba_cart.ld core/*.s core/*.c game/*.c media/map/*.c media/gfx/*.c
+echo "Result == {$?}"
 ${DEVKITARM}/bin/arm-none-eabi-objcopy -O binary hockey2.elf hockey2.gba
 
 cp ./hockey2.gba ../PopUnityBoy/Assets/hockey2.gba.bytes
